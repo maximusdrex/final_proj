a8f5203749207fec1cbf20a4ed2ef73a4c139cd3
student1819 <student1819@ps425248.dreamhostps.com>
2018-12-05 22:40:16 -0800

    Set to use server, fixed PDO

--- api/items/posts.php
+++ api/items/posts.php
@@ -10,8 +10,10 @@ function json_response($output) {
 if($_SERVER["REQUEST_METHOD"] == "GET") {
     if(isset($_GET["page"]) && isset($_GET["pagesize"])) {
         $user_id = $_GET["uid"];
+	# echo "test";
         json_response(array("page" => $_GET["page"], "total" => user_posts_count($user_id), "posts" => select_posts_user_paged($user_id, $_GET["page"], $_GET["pagesize"]) ));    
-    } elseif (isset($_GET["uid"])) {
+    	# echo "test 2";
+} elseif (isset($_GET["uid"])) {
         $user_id = $_GET["uid"];
         json_response(array("posts" => select_posts_user($user_id), "total" => user_posts_count($user_id)));    
     } else {
